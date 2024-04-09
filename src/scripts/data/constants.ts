b3cff0467106349f1a2092a020f5fc672a459ca0
David <david@semke.ca>
2024-04-08 19:56:38 -0700

    Updated rest api url to production url

--- src/scripts/data/constants.ts
+++ src/scripts/data/constants.ts
@@ -1,4 +1,4 @@
 export default Object.freeze({
-    SERVER_URL: 'http://localhost:3000'
-    // SERVER_URL: 'https://deepspeakapi.fly.dev'
+    // SERVER_URL: 'http://localhost:3000'
+    SERVER_URL: 'https://deepspeakapi.fly.dev'
 })
